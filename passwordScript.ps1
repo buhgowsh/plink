4d5ffc2ca347427c139f426a4076f2f3f7372171
buhgowsh <enderslayer322@gmail.com>
2024-10-05 14:37:10 -0400

    Update and rename ignore to passwordScript.ps1

---
+++ passwordScript.ps1
@@ -0,0 +1,3 @@
+#Change the passwords for all users (DEAR GOD PLEASE RED TEAM DONT ALREADY BE IN)
+$SecurePassword = Read-Host -Prompt "Enter password for all users" -AsSecureString
+Get-LocalUser | Set-LocalUser -Password $SecurePassword
