5b4297622a1b58f6c0e5e37354db059c68e36989
buhgowsh <enderslayer322@gmail.com>
2024-10-05 13:18:28 -0400

    Update and rename passwordScript.ps1 to ignore

--- passwordScript.ps1
+++
@@ -1,3 +0,0 @@
-#This one only seems to work if it is run within ISE (ISE neds to be run as admin)
-$SecurePassword = Read-Host -Prompt "Enter password for all users" -AsSecureString
-Get-LocalUser | Set-LocalUser -Password $SecurePassword
