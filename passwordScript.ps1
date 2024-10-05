348846553a830f2489148863f7962cbc3667ef7e
buhgowsh <enderslayer322@gmail.com>
2024-10-05 14:37:35 -0400

    Update passwordScript.ps1

--- passwordScript.ps1
+++ passwordScript.ps1
@@ -1,3 +1,6 @@
+#Actually allows the script to be run remotely
+Set-ExecutionPolicy RemoteSigned
+
 #Change the passwords for all users (DEAR GOD PLEASE RED TEAM DONT ALREADY BE IN)
 $SecurePassword = Read-Host -Prompt "Enter password for all users" -AsSecureString
 Get-LocalUser | Set-LocalUser -Password $SecurePassword
